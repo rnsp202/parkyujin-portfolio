3d06a01ff94a9e175007d78be0d2b7f5f151dd9b
rnsp202 <165612437+rnsp202@users.noreply.github.com>
2024-07-12 18:09:53 +0900

    0712

---
+++ scripts/plugin.js
@@ -0,0 +1,20 @@
+//--------------------------------------fullpage
+$('#wrap').fullpage({
+    scrollBar:true,
+    scrollingSpeed:600,
+    //nav
+    // navigation:true,
+    // navigationTooltips:['표지','소개','sns'],
+    showActiveTooltip:false,
+    // showActiveTooltip:true,
+    // menu:'nav',
+    // anchors:['cover','profile','sns'],
+    // //fullpage(구) 제이쿼리 기능으로 애니메이션도 제이쿼리로 연결해야한다!!
+    // onLeave:function(index, nextIndex, direction){
+    //     console.log(index, nextIndex, direction)
+    //     if(index==2){
+    //         // $('.s3 h1').css('color','lightblue')
+    //         $('.s3 h1').addClass('ani')
+    //     }
+    // }
+})
